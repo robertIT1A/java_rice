7e061dedaa83664c4b5b0ba3a143086810db6388
robertIT1A <robertoguevarrajr25@gmail.com>
2026-03-02 21:41:20 +0800

    json integration in js
    Added JSON handling examples including stringification and parsing.

---
+++ Jason/index.js
@@ -0,0 +1,45 @@
+// JSON = (JavaScript Object Notation) data-interchange format 
+//      Used for exchanging data between a server and a web applicatiob
+//      JSON file {key:value} Or {value1, value2, value3}
+
+// JSON.stringify() = coverts a JS object to a JSON string.
+// JSON.parase() = convert a JSON string to JS object
+
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"]
+const person = `{
+    "name":"Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["Jellyfishing", "Karate", "Cooking"]
+}`
+const people = [{
+   "name":"Spongebob",
+    "age": 30,
+    "isEmployed": true 
+},
+{
+    "name":"Patrick",
+    "age": 34,
+    "isEmployed": false
+},
+{
+    "name":"Squidward",
+    "age": 50,
+    "isEmployed": true
+},
+{
+    "name":"Sandy",
+    "age": 27,
+    "isEmployed": false
+}]
+
+const jsonString = JSON.stringify(people);
+
+console.log(people);
+console.log(jsonString);
+
+
+const parase = JSON.parse(person);
+
+console.log(person);
+console.log(parase);
